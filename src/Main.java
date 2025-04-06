d5ad9936d26bade3dc9cadbb6c323c9bd253f5dc
MPolat <mustfplt4@gmail.com>
2025-04-06 20:35:41 +0300

    post increment example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        postIncrement();
+    }
+
+    public static void postIncrement() {
+        int num1 = 9;
+        int num2 = num1++;
+
+        if (num2 < 10) {
+            System.out.println(num2 + " Hello World");
+        } else {
+            System.out.println(num1 + " Hello Universel");
+        }
     }
 }

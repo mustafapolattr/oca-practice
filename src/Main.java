fee2ba163d2d1aa9e57efd886fe248611946aea0
MPolat <mustfplt4@gmail.com>
2025-04-07 20:37:32 +0300

    int String compare example

--- src/Main.java
+++ src/Main.java
@@ -61,4 +61,13 @@ public class Main {
         }
 
     }
+
+    // Kod (x==s) karşılaştırmasından dolayı derlenemez
+//    public static void intStringCompare() {
+//        int x = 0;
+//        String s = null;
+//
+//        if (x==s) System.out.println("Success");
+//        else System.out.println("Failure");
+//    }
 }

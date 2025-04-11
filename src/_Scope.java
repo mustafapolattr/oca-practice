9e13e20a1582fecc74c1ea50e3bfae755ee4daf0
MPolat <mustfplt4@gmail.com>
2025-04-11 14:13:21 +0300

    scope exampe

---
+++ src/_Scope.java
@@ -0,0 +1,8 @@
+public class _Scope {
+    private static int $;
+    public static void main(String[] args) {
+        String a_b;
+        System.out.println($); // $ değeri class levelde tanımlandığı için default değer atar
+        //System.out.println(a_b); bu satırda hata verir
+    }
+}

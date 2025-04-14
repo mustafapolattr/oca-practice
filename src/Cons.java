180c10f75d294c1a86d3d7c800875074881b2e1b
MPolat <mustfplt4@gmail.com>
2025-04-14 11:24:15 +0300

    static example 1

---
+++ src/Cons.java
@@ -0,0 +1,17 @@
+public class Cons {
+    public static int length=0;
+}
+
+class ConsLength {
+    static Cons cons1 = new Cons();
+    static Cons cons2 = new Cons();
+    static {
+        System.out.print(cons1.length);
+    }
+
+    public static void main(String[] args) {
+        cons1.length=2;
+        cons2.length=8;
+        System.out.println(cons1.length);
+    }
+}
